3813bc0d4c26270052d2e63dcc4e29117707fc84
rescDev <r.schach96@gmail.com>
2020-12-22 11:23:15 +0100

    Add files

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get config from environment variables and prepare run
+cmd=""
+
+if [ -n "$BLACKLIST" ]; then
+    cmd="$cmd --blacklist=$BLACKLIST"
+fi
+
+if [ -n "$DISABLED" ]; then
+    cmd="$cmd --disable=$DISABLED"
+fi
+
+if [ -n "$OUTPUT" ]; then
+    arrOutput=("${OUTPUT//,/ }")
+    sorted=($(echo "${arrOutput[@]}"| tr " " "\n" | sort -n | tr "\n" " "))
+    if [ "${sorted[0]}" == "debug" ]; then
+        cmd="$cmd --debug"
+    fi
+    if [ "${sorted[1]}" == "verbose" ]; then
+        cmd="$cmd --verbose"
+    fi
+fi
+
+# Trim spaces
+cmd=$(echo "$cmd" | xargs)
+
+# Run go-mutesting
+echo "Will run mutation tests with $cmd on $TARGETS"
+go-mutesting "$TARGETS" "$cmd"
